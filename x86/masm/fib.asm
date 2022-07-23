ee8d921d07fe2d19f1f217c6e1743a8a8e4be65a
TareqRafed <tareq.rafed22@gmail.com>
2022-07-23 03:00:51 +0300

    init w fib

---
+++ x86/masm/fib.asm
@@ -0,0 +1,70 @@
+TITLE Fib algorithm
+
+COMMENT !
+
+Return fib(n); where n is the input
+
+; TODO: handle negative values
+
+!
+
+INCLUDE Irvine32.inc
+
+.data
+intNum    DWORD ?
+promptBad BYTE "Invalid input, please enter again", 0dh, 0ah ,0
+
+.code
+main PROC
+
+    read:  
+           call ReadInt
+           jno  goodInput
+
+           mov  edx,OFFSET promptBad
+           call WriteString
+           jmp  read        ;go input again
+
+    goodInput:
+           mov  intNum,eax  ;store good value
+
+
+    initFib:
+			COMMENT !
+				two pointers; where eax is the left Pointer and ebx is the right pointer
+			!
+            mov eax, 0 ; manual init
+            mov ebx, 1 ; manual init 
+
+            mov ecx, 0 ; counter
+            mov edx, 0 ; tmp
+
+            ; edge case whenever input is 0
+            cmp intNum, eax
+            je caseZero
+
+    startFib:
+            mov edx, ebx ; temp store, to move to eax later 
+            add ebx, eax ; store sum value in the right pointer
+            mov eax, edx ; assign left value, to old right
+
+            inc ecx
+			
+            cmp ecx, intNum ; terminate func if reached user input
+            je printResult
+            jmp startFib
+
+
+    printResult:
+        	call WriteInt
+        	exit
+       
+
+    caseZero:
+            mov eax, 0
+            call WriteInt
+            exit
+
+	exit
+main ENDP
+END main
